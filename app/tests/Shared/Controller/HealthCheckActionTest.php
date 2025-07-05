371691009559a723e08a72a081de5970094315ee
nikolay vanzhin <nikolay.vanzhin@yandex.ru>
2025-07-05 16:10:40 +0500

    feat. phpunit started

---
+++ app/tests/Shared/Controller/HealthCheckActionTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Shared\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+
+class HealthCheckActionTest extends WebTestCase
+{
+    public function testRequestRespondedSuccessfulResult(): void
+    {
+        //arrange
+        $client = static::createClient();
+        $client->request(Request::METHOD_GET, '/health-check');
+
+        //act
+        $jsonResult = json_decode($client->getResponse()->getContent(), true);
+
+        //assert
+        $this->assertResponseIsSuccessful();
+        $this->assertEquals($jsonResult['data']['status'], 'ok');
+    }
+}
